65f1ef62e07376f6a7920cc6392e5f24a3e4562b
worldallmovieflix <154462058+worldallmovieflix@users.noreply.github.com>
2023-12-26 15:12:32 +0530

    Create sample_info.py

---
+++ sample_info.py
@@ -0,0 +1,46 @@
+# Bot information
+
+SESSION = 'Media_search'
+
+USER_SESSION = 'User_Bot'
+
+API_ID = 28511116
+
+API_HASH = '5acd24be93590c566c6a496a3094606e'
+
+BOT_TOKEN = '6778879309:AAFZkjslZzDaP2kJqIKzwDSeSO2ZLh1R9KA'
+
+USERBOT_STRING_SESSION = ''
+
+
+
+# Bot settings
+
+CACHE_TIME = 300
+
+USE_CAPTION_FILTER = False
+
+
+
+# Admins, Channels & Users
+
+ADMINS = []
+
+CHANNELS = []
+
+AUTH_USERS = []
+
+AUTH_CHANNEL = None
+
+
+
+# MongoDB information
+
+DATABASE_URI = "mongodb+srv://wasimakram@cluster0.2kh2yzb.mongodb.net/?retryWrites=true&w=majority"
+
+DATABASE_NAME = 'Cluster0'
+
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+
+
